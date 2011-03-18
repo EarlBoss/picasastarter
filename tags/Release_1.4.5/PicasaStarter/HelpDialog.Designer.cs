675ad5ea3f48c90f6533885f292cac70e18db370
pieter.roggemans@gmail.com <pieter.roggemans@gmail.com@1678de8e-22d6-fd14-0533-fbbc9ae01a67>
2011-03-18 22:31:39 +0000

    Tag for release 1.4.5

---
+++ tags/Release_1.4.5/PicasaStarter/HelpDialog.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PicasaStarter
+{
+    partial class HelpDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.richTextBoxHelp = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(618, 449);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // richTextBoxHelp
+            // 
+            this.richTextBoxHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxHelp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxHelp.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxHelp.Name = "richTextBoxHelp";
+            this.richTextBoxHelp.ReadOnly = true;
+            this.richTextBoxHelp.Size = new System.Drawing.Size(681, 431);
+            this.richTextBoxHelp.TabIndex = 2;
+            this.richTextBoxHelp.Text = "Hier komt de help text...";
+            // 
+            // HelpDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 484);
+            this.Controls.Add(this.richTextBoxHelp);
+            this.Controls.Add(this.buttonClose);
+            this.Name = "HelpDialog";
+            this.Text = "Help";
+            this.Load += new System.EventHandler(this.HelpDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.RichTextBox richTextBoxHelp;
+    }
+}
