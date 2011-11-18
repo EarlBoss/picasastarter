5be09804b6b412b77f308211d6bed263e799bc5f
earlboss <earlboss@1678de8e-22d6-fd14-0533-fbbc9ae01a67>
2011-11-18 22:23:57 +0000

    Added Updates to the Virtual Drive code - Will be branched off to PicasaStarterV (V = Virtual Drive) The Implementation is not robust yet, and I don't want it cluttering up the Trunk revision.  Will try to merge it in later when it works better.

--- PicasaStarter/1-Views/MainForm.Designer.cs
+++ PicasaStarter/1-Views/MainForm.Designer.cs
@@ -217,6 +217,7 @@
             this.listBoxPicasaDBs.Name = "listBoxPicasaDBs";
             this.listBoxPicasaDBs.Size = new System.Drawing.Size(200, 147);
             this.listBoxPicasaDBs.TabIndex = 43;
+             this.listBoxPicasaDBs.SelectedIndexChanged += new System.EventHandler(this.listBoxPicasaDBs_SelectedIndexChanged);
             this.listBoxPicasaDBs.DoubleClick += new System.EventHandler(this.listBoxPicasaDBs_DoubleClick);
             this.listBoxPicasaDBs.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnListBoxMouseMove);
             // 
