654af3dfe10067e890387aeeadf21fe6b58d9e0b
pieter.roggemans@gmail.com <pieter.roggemans@gmail.com@1678de8e-22d6-fd14-0533-fbbc9ae01a67>
2012-01-23 00:21:53 +0000

    - Integrate some changes from 1.7 branch - Split First run wizard in 2 steps...

---
+++ branches/PicasaStarterV Ver2.0.0.9/PicasaStarter/1-Views/FirstRunWizardStep1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;                    // Added to use Path class
+
+namespace PicasaStarter
+{
+    public partial class FirstRunWizardStep1 : Form
+    {
+        public string PicasaExePath { get; private set; }
+        
+        public FirstRunWizardStep1(string defaultPicasaExePath)
+        {
+            InitializeComponent();
+
+            PicasaExePath = defaultPicasaExePath;
+        }
+
+        private void FirstRunWizardStep1_Load(object sender, EventArgs e)
+        {
+            textBoxPicasaExePath.Text = PicasaExePath;
+        }
+
+        private void buttonBrowsePicasaExePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Path.GetDirectoryName(PicasaExePath);
+            openFileDialog1.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPicasaExePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            PicasaExePath = textBoxPicasaExePath.Text;
+
+            if (!File.Exists(PicasaExePath))
+            {
+                MessageBox.Show("Please choose a path where Picasa.exe can be found, or cancel and install Picasa first from picasa.google.com");
+                return;
+            }
+            
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
---
+++ branches/PicasaStarterV Ver2.0.0.9/PicasaStarter/1-Views/FirstRunWizardStep2.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PicasaStarter
+{
+    partial class FirstRunWizardStep2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FirstRunWizardStep2));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ButtonSelXMLPath = new System.Windows.Forms.Button();
+            this.ButtonSetXMLToDef = new System.Windows.Forms.Button();
+            this.textBoxSettingsXMLPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.ButtonSelXMLPath);
+            this.groupBox2.Controls.Add(this.ButtonSetXMLToDef);
+            this.groupBox2.Controls.Add(this.textBoxSettingsXMLPath);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(552, 303);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PicasaStarterSettings.xml File Path";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(305, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Browse to location of the desired settings file or select Default...";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(493, 169);
+            this.label6.TabIndex = 25;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // ButtonSelXMLPath
+            // 
+            this.ButtonSelXMLPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSelXMLPath.Location = new System.Drawing.Point(390, 272);
+            this.ButtonSelXMLPath.Name = "ButtonSelXMLPath";
+            this.ButtonSelXMLPath.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSelXMLPath.TabIndex = 20;
+            this.ButtonSelXMLPath.Text = "Browse...";
+            this.ButtonSelXMLPath.UseVisualStyleBackColor = true;
+            this.ButtonSelXMLPath.Click += new System.EventHandler(this.ButtonSelXMLPath_Click);
+            // 
+            // ButtonSetXMLToDef
+            // 
+            this.ButtonSetXMLToDef.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSetXMLToDef.Location = new System.Drawing.Point(471, 273);
+            this.ButtonSetXMLToDef.Name = "ButtonSetXMLToDef";
+            this.ButtonSetXMLToDef.Size = new System.Drawing.Size(75, 22);
+            this.ButtonSetXMLToDef.TabIndex = 19;
+            this.ButtonSetXMLToDef.Text = "Default";
+            this.ButtonSetXMLToDef.UseVisualStyleBackColor = true;
+            this.ButtonSetXMLToDef.Click += new System.EventHandler(this.ButtonSetXMLToDef_Click);
+            // 
+            // textBoxSettingsXMLPath
+            // 
+            this.textBoxSettingsXMLPath.AccessibleDescription = "";
+            this.textBoxSettingsXMLPath.Enabled = false;
+            this.textBoxSettingsXMLPath.Location = new System.Drawing.Point(15, 274);
+            this.textBoxSettingsXMLPath.Name = "textBoxSettingsXMLPath";
+            this.textBoxSettingsXMLPath.Size = new System.Drawing.Size(369, 20);
+            this.textBoxSettingsXMLPath.TabIndex = 18;
+            this.textBoxSettingsXMLPath.Tag = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Path to picasa starter database settings (.xml):";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Finish";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(422, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Note: These settings can be changed later by clicking on the \"General Settings\" b" +
+    "utton.";
+            // 
+            // FirstRunWizardStep2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "FirstRunWizardStep2";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FirstRunWizardStep2";
+            this.Load += new System.EventHandler(this.FirstRunWizardStep2_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ButtonSelXMLPath;
+        private System.Windows.Forms.Button ButtonSetXMLToDef;
+        private System.Windows.Forms.TextBox textBoxSettingsXMLPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+    }
+}
